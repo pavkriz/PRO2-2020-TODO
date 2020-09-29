cb2f12506aa59500d849aef2e374baf167f5bcfb
vahalma2 <vahalma2@uhk.cz>
2020-09-29 13:04:30 +0200

    Novy commit

--- src/main/java/cz/uhk/pro2/todo/TodoMain.java
+++ src/main/java/cz/uhk/pro2/todo/TodoMain.java
@@ -2,6 +2,10 @@ package cz.uhk.pro2.todo;
 
 public class TodoMain {
     public static void main(String[] args) {
-        System.out.println("Hello!!!");
+        String zkouska = "Zkouska - novy push";
+        System.out.printf("%s", zkouska);
     }
 }
+
+// push do gitu vzdy pres branch vahalma2 (dole vpravo) - NE MASTER!!!
+// VCS -> Git -> Push
