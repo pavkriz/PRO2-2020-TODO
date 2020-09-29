2dcac3ce3fb9bdb3a5f8b3feaee36b28b46c7db7
chrasjo1 <chrasjo1@uhk.cz>
2020-09-29 13:04:38 +0200

    comm2

--- src/main/java/cz/uhk/pro2/todo/TodoMain.java
+++ src/main/java/cz/uhk/pro2/todo/TodoMain.java
@@ -2,6 +2,6 @@ package cz.uhk.pro2.todo;
 
 public class TodoMain {
     public static void main(String[] args) {
-        System.out.println("Hello!!!....");
+        System.out.println("Hello!!!.......");
     }
 }
