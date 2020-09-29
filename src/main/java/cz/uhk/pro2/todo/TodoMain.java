182a02beaa315a51b2bfdef2efe6bb1c9048dc9b
pavkriz <pavkriz@hkfree.org>
2020-09-29 12:31:46 +0200

    Initial commit

---
+++ src/main/java/cz/uhk/pro2/todo/TodoMain.java
@@ -0,0 +1,7 @@
+package cz.uhk.pro2.todo;
+
+public class TodoMain {
+    public static void main(String[] args) {
+        System.out.println("Hello!!!");
+    }
+}
---
+++ src/test/java/cz/uhk/pro2/todo/TodoMainTest.java
@@ -0,0 +1,14 @@
+package cz.uhk.pro2.todo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TodoMainTest {
+    @Test
+    public void test1() {
+        // priprava + operace
+        String result = "abc";
+        assertEquals(result, "abc");
+    }
+}
