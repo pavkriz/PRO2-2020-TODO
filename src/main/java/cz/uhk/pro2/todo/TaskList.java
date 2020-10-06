badf4506bbc2c6752dbb862b117a31bc42966171
Filip Troksiar <filip.troksiar@gmail.com>
2020-10-06 12:57:51 +0200

    initial commit

--- src/main/java/cz/uhk/pro2/todo/TaskList.java
+++ src/main/java/cz/uhk/pro2/todo/TaskList.java
@@ -3,12 +3,13 @@ package cz.uhk.pro2.todo;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 public class TaskList {
 
     private List<Task> tasks = new ArrayList<>();
 
-    public List<Task> getTask(){
+    public List<Task> getTasks(){
         return Collections.unmodifiableList(tasks); //vrati nemenny list
     }
 
@@ -19,5 +20,4 @@ public class TaskList {
     public void removeTask(Task t){
         tasks.remove(t);
     }
-
 }
