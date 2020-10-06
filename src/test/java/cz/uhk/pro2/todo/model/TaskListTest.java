1e06fa4ef414294656f13785fbc3c13db462a19b
pavkriz <pavkriz@hkfree.org>
2020-10-06 12:14:13 +0200

    TaskListTest

---
+++ src/test/java/cz/uhk/pro2/todo/model/TaskListTest.java
@@ -0,0 +1,18 @@
+package cz.uhk.pro2.todo.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TaskListTest {
+    @Test
+    public void testAddTask() {
+        TaskList l = new TaskList();
+        Task t = new Task();
+        assertEquals(0, l.getTasks().size());
+        l.addTask(t);
+        assertEquals(1, l.getTasks().size());
+        assertEquals(t, l.getTasks().get(0));
+    }
+
+}
