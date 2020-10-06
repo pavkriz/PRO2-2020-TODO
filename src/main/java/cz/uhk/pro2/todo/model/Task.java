98ad3d02b1868cf9c4770a12b9723d9667fab7b4
plechmi2 <plechmi2@uhk.cz>
2020-10-06 12:16:23 +0200

    ini

---
+++ src/main/java/cz/uhk/pro2/todo/model/Task.java
@@ -0,0 +1,6 @@
+package cz.uhk.pro2.todo.model;
+
+public class Task {
+    private String description;
+    private boolean done;
+}
---
+++ src/main/java/cz/uhk/pro2/todo/model/TaskList.java
@@ -0,0 +1,19 @@
+package cz.uhk.pro2.todo.model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class TaskList {
+    private List<Task> tasks = new ArrayList<>();
+
+    public List<Task> getTasks(){
+        return Collections.unmodifiableList(tasks);
+    }
+    public void addTask(Task t){
+        tasks.add(t);
+    }
+    public void removeTask(Task t){
+        tasks.remove(t);
+    }
+}
