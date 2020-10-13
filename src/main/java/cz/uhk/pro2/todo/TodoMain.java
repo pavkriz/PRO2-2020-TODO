bff1ee44e22a46f8d021e7fe42736f1f37e989d9
Meykr <MeykrPlay@gmail.com>
2020-10-13 12:52:33 +0200

    Po hodine 13.10 dodelat ukol do pristiho tydne.

--- src/main/java/cz/uhk/pro2/todo/TodoMain.java
+++ src/main/java/cz/uhk/pro2/todo/TodoMain.java
@@ -1,15 +1,19 @@
 package cz.uhk.pro2.todo;
 
 import cz.uhk.pro2.todo.gui.TasksTableModel;
+import cz.uhk.pro2.todo.model.Task;
+import cz.uhk.pro2.todo.model.TaskList;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Date;
 
 public class TodoMain extends JFrame{
 
     private JButton btnAdd = new JButton("Přidat úkol");
     private JPanel pnlNorth = new JPanel();
-    private TasksTableModel tasksTableModel = new TasksTableModel();
+    private TaskList taskList = new TaskList();
+    private TasksTableModel tasksTableModel = new TasksTableModel(taskList);
     private JTable tbl = new JTable(tasksTableModel);
 
     public TodoMain() throws HeadlessException{
@@ -18,12 +22,22 @@ public class TodoMain extends JFrame{
         pnlNorth.add(btnAdd);
         add(pnlNorth, BorderLayout.NORTH);
         add(tbl, BorderLayout.CENTER);
-        pack();
         btnAdd.addActionListener(e -> addTask());
+       /*
+        taskList.addTask(new Task("Naucit se Javu", new Date(), false));
+        taskList.addTask(new Task("Behat", new Date(), false));
+        taskList.addTask(new Task("Nakoupit", new Date(), false));
+        taskList.addTask(new Task("Vyvarit rousku", new Date(), false));
+        taskList.addTask(new Task("Naucit se Javu", new Date(), false));
+       */
+        pack();
     }
 
     private void addTask(){
         //TODO
+        String taskString = JOptionPane.showInputDialog("Zadej ulohu");
+        taskList.addTask(new Task(taskString, new Date(), false));
+        //notifikujeme tabulku ze doslo ke zmene dat
     }
 
     public static void main(String[] args) {
--- src/main/java/cz/uhk/pro2/todo/gui/TasksTableModel.java
+++ src/main/java/cz/uhk/pro2/todo/gui/TasksTableModel.java
@@ -1,20 +1,48 @@
 package cz.uhk.pro2.todo.gui;
 
+import cz.uhk.pro2.todo.model.Task;
+import cz.uhk.pro2.todo.model.TaskList;
+
 import javax.swing.table.AbstractTableModel;
 
 public class TasksTableModel extends AbstractTableModel{
+
+    private TaskList taskList;
+
+    public TasksTableModel(TaskList taskList){
+        this.taskList = taskList;
+
+    }
+
     @Override
     public int getRowCount() {
-        return 3;
+        return taskList.getTasks().size();
     }
 
     @Override
     public int getColumnCount() {
-        return 2;
+        return 3;
     }
 
     @Override
-    public Object getValueAt(int i, int i1) {
-        return "ABC";
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Task task = taskList.getTasks().get(rowIndex);
+        switch (columnIndex){
+            case 0: return task.getDescription();
+            case 1: return task.getDueDate();
+            case 2: return task.isDone();
+        }
+        return ""; // nikdy by se nemelo stat
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        Task task = taskList.getTasks().get(column);
+        switch (column){
+            case 0: return "Úloha";
+            case 1: return "Datum";
+            case 2: return "Stav úlohy";
+        }
+        return ""; // nikdy by se nemelo stat
     }
 }
