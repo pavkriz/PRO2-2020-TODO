836fe997cfd70e6596059d15fc85348b5eab3f82
pavkriz <pavkriz@hkfree.org>
2020-10-13 11:13:31 +0200

    Task, TaskList, Tests

---
+++ src/main/java/cz/uhk/pro2/todo/model/Task.java
@@ -0,0 +1,51 @@
+package cz.uhk.pro2.todo.model;
+
+import java.util.Date;
+
+public class Task {
+    private String description;
+    private Date dueDate;
+    private boolean done;
+
+    public Task() {
+    }
+
+    public Task(String description, Date dueDate, boolean done) {
+        this.description = description;
+        this.dueDate = dueDate;
+        this.done = done;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "description='" + description + '\'' +
+                ", dueDate=" + dueDate +
+                ", done=" + done +
+                '}';
+    }
+}
