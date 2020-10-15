09c8e19cca5390f010c4b12ac21caa08cd600210
hladiji2 <hladas250@email.cz>
2020-10-15 10:40:28 +0200

    Dokončení DU1

--- src/main/java/cz/uhk/pro2/todo/TodoMain.java
+++ src/main/java/cz/uhk/pro2/todo/TodoMain.java
@@ -6,8 +6,8 @@ import cz.uhk.pro2.todo.model.TaskList;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class TodoMain extends JFrame {
@@ -16,6 +16,8 @@ public class TodoMain extends JFrame {
     private TaskList taskList = new TaskList();
     private TasksTableModel tasksTableModel = new TasksTableModel(taskList);
     private JTable tbl = new JTable(tasksTableModel);
+    private JFrame f;
+
 
     public TodoMain() throws HeadlessException {
         setTitle("TODO app");
@@ -32,19 +34,35 @@ public class TodoMain extends JFrame {
 
     private void addTask() {
         // TODO DU1
+
+        f = new JFrame();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.mm HH:mm");
+
         // zeptame se uzivatele
-        // vytvorime task a pridame do seznamu
+        String jmeno = JOptionPane.showInputDialog(f,"Zadej jméno úkolu");
+        String datum = JOptionPane.showInputDialog(f,"Zadej konečné datum formátu dd.mm HH:mm");
+        Date dueDate;
+        try {
+            // Přeformátujeme uživatel přidaný string na daný formát
+            dueDate = sdf.parse(datum);
+            // vytvorime task a pridame do seznamu
+            taskList.addTask(new Task(jmeno, dueDate, false));
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
         // notifikujeme tabulku, ze doslo ze zmene dat
+        tasksTableModel.fireTableDataChanged();
     }
+
+
 
     public static void main(String[] args) {
         //System.out.println("Hello!!!");
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                TodoMain window = new TodoMain();
-                window.setVisible(true);
-            }
+        SwingUtilities.invokeLater(() -> {
+            TodoMain window = new TodoMain();
+            window.setVisible(true);
         });
     }
 }
--- src/main/java/cz/uhk/pro2/todo/gui/TasksTableModel.java
+++ src/main/java/cz/uhk/pro2/todo/gui/TasksTableModel.java
@@ -4,6 +4,7 @@ import cz.uhk.pro2.todo.model.Task;
 import cz.uhk.pro2.todo.model.TaskList;
 
 import javax.swing.table.AbstractTableModel;
+import java.text.SimpleDateFormat;
 
 public class TasksTableModel extends AbstractTableModel {
     private TaskList taskList;
@@ -24,11 +25,14 @@ public class TasksTableModel extends AbstractTableModel {
 
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
+
         Task task = taskList.getTasks().get(rowIndex);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM HH:mm");
+
         switch (columnIndex) {
             case 0: return task.getDescription();
-            case 1: return task.getDueDate();
-            case 2: return task.isDone();
+            case 1: return sdf.format(task.getDueDate());
+            case 2: return task.isDone()?"x":"";
         }
         return ""; // tohle by se nemelo volat
     }
@@ -36,6 +40,12 @@ public class TasksTableModel extends AbstractTableModel {
     @Override
     public String getColumnName(int column) {
         // TODO DU2
-        return "Nadpis";
+        switch (column) {
+            case 0: return "Popis";
+            case 1: return "Platnost";
+            case 2: return "Splnění";
+        }
+        return ""; // tohle by se nemelo volat
     }
+
 }
