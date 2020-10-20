3ddfdc6f158833318871cefd9e29d6c19f65bd08
David <david.cesak@uhk.cz>
2020-10-20 12:06:12 +0200

    Homework Week 04

--- src/main/java/cz/uhk/pro2/todo/TodoMain.java
+++ src/main/java/cz/uhk/pro2/todo/TodoMain.java
@@ -1,17 +1,29 @@
 package cz.uhk.pro2.todo;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import cz.uhk.pro2.todo.gui.TasksTableModel;
 import cz.uhk.pro2.todo.model.Task;
 import cz.uhk.pro2.todo.model.TaskList;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class TodoMain extends JFrame {
 
     private final JButton btnAddTask = new JButton("Přidat úkol");
+
+    private final JButton btnRemoveTask = new JButton("Odebrat úkol");
+
+    private final JButton btnSaveToFile = new JButton("Ulož data do souboru");
+
+    private final JLabel lblUnfinishedTasks = new JLabel("Počet nesplněných úkolů: 0");
 
     private final JPanel pnlNorth = new JPanel();
 
@@ -25,12 +37,53 @@ public class TodoMain extends JFrame {
         setTitle("TODO aplikace");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         pnlNorth.add(btnAddTask);
+        pnlNorth.add(btnRemoveTask);
+        pnlNorth.add(lblUnfinishedTasks);
         add(pnlNorth, BorderLayout.NORTH);
         add(new JScrollPane(tbl), BorderLayout.CENTER);
+        add(btnSaveToFile, BorderLayout.SOUTH);
         pack();
-        btnAddTask.addActionListener(e -> addTask());
+        btnAddTask.addActionListener(e -> {
+            addTask();
+            updateUnfinishedTasks();
+        });
+        btnRemoveTask.addActionListener(e -> {
+            removeHighlightedTask();
+            updateUnfinishedTasks();
+        });
+        btnSaveToFile.addActionListener(e -> saveToFile());
     }
 
+    private void updateUnfinishedTasks() {
+        lblUnfinishedTasks.setText("Počet nesplněných úkolů: " + taskList.getUndoneTasksCount());
+    }
+
+    private void removeHighlightedTask() {
+        int selectedRow = tbl.getSelectedRow();
+        if (selectedRow != -1) {
+            taskList.removeTask(selectedRow);
+        }
+        tbl.updateUI();
+    }
+
+    private void saveToFile() {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileFilter(new FileNameExtensionFilter("JSON soubory", "json"));
+        fileChooser.setDialogTitle("Vyberte, kam uložit soubor s daty");
+        Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX").create();
+
+        if (fileChooser.showSaveDialog(this) == JFileChooser.APPROVE_OPTION) {
+            File fileToSave = fileChooser.getSelectedFile();
+            try {
+                FileWriter fw = new FileWriter(fileToSave);
+                fw.write(gson.toJson(taskList));
+                fw.close();
+            }
+            catch (Throwable throwable) {
+                JOptionPane.showMessageDialog(this, "Nepodařilo se uložit soubor.");
+            }
+        }
+    }
 
     private void addTask() {
         String description = JOptionPane.showInputDialog(this, "Zadej název úkolu");
@@ -51,7 +104,7 @@ public class TodoMain extends JFrame {
         }
 
         Object isFinishedObject = JOptionPane.showInputDialog(this, "Byl už úkol splněn?",
-                "Splění úkolu", JOptionPane.QUESTION_MESSAGE,
+                "Splnění úkolu", JOptionPane.QUESTION_MESSAGE,
                 null, new Object[]{"ANO", "NE"}, "NE");
         if (isFinishedObject == null) {
             return;
