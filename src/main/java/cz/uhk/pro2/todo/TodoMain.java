d6c2e9c80a0f669d264d86bf1490d7a9c0efa00c
pahulpa1 <tiragoshi@gmail.com>
2020-11-02 19:40:01 +0100

    Domaci ukol z 27.10.2020

--- src/main/java/cz/uhk/pro2/todo/TodoMain.java
+++ src/main/java/cz/uhk/pro2/todo/TodoMain.java
@@ -14,6 +14,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class TodoMain extends JFrame {
@@ -26,7 +28,7 @@ public class TodoMain extends JFrame {
     private JButton btnDel = new JButton("Odstranit úkol");
     private JButton btnSave = new JButton("Ulozit do JSON");
 
-    public TodoMain() throws HeadlessException, IOException {
+    public TodoMain() throws HeadlessException, IOException, ParseException {
         setTitle("TODO app");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         pnlNorth.add(btnAdd);
@@ -36,7 +38,14 @@ public class TodoMain extends JFrame {
         add(pnlNorth, BorderLayout.NORTH);
         add(new JScrollPane(tbl), BorderLayout.CENTER);
         pack();
-        btnAdd.addActionListener(e -> addTask());
+        btnAdd.addActionListener(e -> {
+            try {
+                addTask();
+            } catch (ParseException e2) {
+                // TODO Auto-generated catch block
+                e2.printStackTrace();
+            }
+        });
         btnDel.addActionListener(e -> deleteTask());
         btnSave.addActionListener(e -> {
             try {
@@ -45,10 +54,18 @@ public class TodoMain extends JFrame {
                 System.out.println(e1);
             }
         });
-        taskList.addTask(new Task("Naučit se Javu", new Date(), false));
-        taskList.addTask(new Task("Jit se proběhnout", new Date(), false));
-        taskList.addTask(new Task("Vyvařit roušku", new Date(), false));
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy HH:mm");
+        Date d = sdf.parse("25.11.2020 23:59");
+        taskList.addTask(new Task("Naučit se Javu", d, false));
+        taskList.addTask(new Task("Jit se proběhnout", d, false));
+        taskList.addTask(new Task("Vyvařit roušku", d, false));
         lblUndoneTasks.setText("Undone tasks: " + taskList.getUndoneTasksCount());
+        Timer timer  = new Timer(10000, e -> {
+            tbl.revalidate();
+            tbl.repaint();
+            
+        });
+        timer.start();
     }
 
     private void saveJson() throws IOException {
@@ -68,22 +85,31 @@ public class TodoMain extends JFrame {
     }
 
     private void deleteTask() {
-
+        if(tbl.getSelectedRow()!=-1){
         taskList.removeTask(taskList.getTasks().get(tbl.getSelectedRow()));
         lblUndoneTasks.setText("Undone tasks: " + taskList.getUndoneTasksCount());
         tbl.revalidate();
-        tbl.repaint();
+        tbl.repaint();}
     }
 
-    private void addTask() {
+    private void addTask() throws ParseException {
         // TODO DU1
         // zeptame se uzivatele
         // vytvorime task a pridame do seznamu
         // notifikujeme tabulku, ze doslo ze zmene dat
         JOptionPane jo = new JOptionPane();
         String des = jo.showInputDialog("Zadejte description");
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy HH:mm");
+        Date d = new Date();
+         try {
+            String date=jo.showInputDialog("Zadejte datum ve tvaru: 'dd.MM.yyyy HH:mm'");
+            d = sdf.parse(date);
+        } catch (HeadlessException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } 
         if (des != "" || des != null) {
-            taskList.addTask(new Task(des, new Date(), false));
+            taskList.addTask(new Task(des, d, false));
         }
         lblUndoneTasks.setText("Undone tasks: " + taskList.getUndoneTasksCount());
         tbl.revalidate();
@@ -104,6 +130,9 @@ public class TodoMain extends JFrame {
                     // TODO Auto-generated catch block
                     e.printStackTrace();
                 } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (ParseException e) {
                     // TODO Auto-generated catch block
                     e.printStackTrace();
                 }
--- src/main/java/cz/uhk/pro2/todo/gui/TasksTableModel.java
+++ src/main/java/cz/uhk/pro2/todo/gui/TasksTableModel.java
@@ -3,6 +3,9 @@ package cz.uhk.pro2.todo.gui;
 import cz.uhk.pro2.todo.model.Task;
 import cz.uhk.pro2.todo.model.TaskList;
 
+
+import java.util.Date;
+
 import javax.swing.table.AbstractTableModel;
 
 public class TasksTableModel extends AbstractTableModel {
@@ -19,16 +22,19 @@ public class TasksTableModel extends AbstractTableModel {
 
     @Override
     public int getColumnCount() {
-        return 3;
+        return 4;
     }
 
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
         Task task = taskList.getTasks().get(rowIndex);
+        
+        Date date = new Date();
         switch (columnIndex) {
             case 0: return task.getDescription();
             case 1: return task.getDueDate();
             case 2: return task.isDone();
+            case 3:return (task.getDueDate().getTime()-date.getTime())/ 86400000;
         }
         return ""; // tohle by se nemelo volat
     }
@@ -39,6 +45,7 @@ public class TasksTableModel extends AbstractTableModel {
             case 0:return "Description";
             case 1:return "Due date";
             case 2:return "Is task done?";
+            case 3:return "Remaining days";
             default: return "";
         } // default
         
