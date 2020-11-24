e94a4de755803811eab0cec03d18661170e149c3
borzisi1 <borzisi1@uhk.cz>
2020-11-24 11:51:04 +0100

    TODO

--- src/main/java/cz/uhk/pro2/todo/TodoMain.java
+++ src/main/java/cz/uhk/pro2/todo/TodoMain.java
@@ -6,12 +6,9 @@ import cz.uhk.pro2.todo.model.TaskList;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class TodoMain extends JFrame {
     private JButton btnAdd = new JButton("Přidat úkol");
